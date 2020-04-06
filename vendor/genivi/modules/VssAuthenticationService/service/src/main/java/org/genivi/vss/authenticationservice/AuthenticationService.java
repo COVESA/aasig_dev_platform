a0e300129ecf12ddc1a83f952a62a083ea78b030
Stefan Wysocki <stefan.wysocki@tietoevry.com>
2020-04-06 17:49:09 +0200

    Service module sceleton

---
+++ vendor/genivi/modules/VssAuthenticationService/service/src/main/java/org/genivi/vss/authenticationservice/AuthenticationService.java
@@ -0,0 +1,15 @@
+package org.genivi.vss.authenticationservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class AuthenticationService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
