60fb6beaeb1bbcdcd87d81e1d9d4cc594050d857
Stefan Wysocki <stefan.wysocki@tietoevry.com>
2020-04-09 12:55:52 +0200

    Connect client with service

--- vendor/genivi/modules/VssAuthenticationService/ExampleAppJava/src/main/java/org/genivi/vss/authenticationservice/MainActivity.java
+++ vendor/genivi/modules/VssAuthenticationService/ExampleAppJava/src/main/java/org/genivi/vss/authenticationservice/MainActivity.java
@@ -2,13 +2,38 @@ package org.genivi.vss.authenticationservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 
 public class MainActivity extends AppCompatActivity {
+
+    IAuthenticationService mAuthenticationService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO Move this connection boilerplate to VSS-SDK since this code will be shared across the clients
+        bindService(VSS.AUTHENTICATION_SERVICE_INTENT, new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                mAuthenticationService = IAuthenticationService.Stub.asInterface(service);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                mAuthenticationService = null;
+            }
+        }, BIND_AUTO_CREATE);
+
+        // TODO connect "getAuthToken" to some controls
+    }
+
+    private String getAuthToken() throws RemoteException {
+        return mAuthenticationService.getAuthenticationToken();
     }
 }
--- vendor/genivi/modules/VssAuthenticationService/service/src/main/java/org/genivi/vss/authenticationservice/IAuthenticationServiceImpl.java
+++ vendor/genivi/modules/VssAuthenticationService/service/src/main/java/org/genivi/vss/authenticationservice/IAuthenticationServiceImpl.java
@@ -1,11 +1,27 @@
 package org.genivi.vss.authenticationservice;
 
+import android.content.Context;
 import android.os.RemoteException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IAuthenticationServiceImpl extends IAuthenticationService.Stub {
+    private final Context mContext;
+
+    IAuthenticationServiceImpl(Context context) {
+        mContext = context;
+    }
+
     @Override
     public String getAuthenticationToken() throws RemoteException {
-        // TODO Connect to PackageMAnager and generate token
+        List<String>  grantedPermissions = getGrantedPermissions();
+        // TODO Generate JWT with fetched permisions
         return null;
+    }
+
+    private List<String> getGrantedPermissions() {
+        // TODO Connect to PackageManager and retrieve permissions;
+        return new ArrayList<String>();
     }
 }
