e96a4dde5b04ef0f9f9fbc28b6cb40dc50382825
Stefan Wysocki <stefan.wysocki@tietoevry.com>
2020-06-15 17:02:00 +0200

    Add permission retrieval from PM

--- vendor/genivi/modules/VssAuthenticationService/service/src/main/java/org/genivi/vss/authenticationservice/IAuthenticationServiceImpl.java
+++ vendor/genivi/modules/VssAuthenticationService/service/src/main/java/org/genivi/vss/authenticationservice/IAuthenticationServiceImpl.java
@@ -6,12 +6,18 @@
 package org.genivi.vss.authenticationservice;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Binder;
 import android.os.RemoteException;
+import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class IAuthenticationServiceImpl extends IAuthenticationService.Stub {
+    private final static String TAG = IAuthenticationServiceImpl.class.getSimpleName();
     private final Context mContext;
 
     IAuthenticationServiceImpl(Context context) {
@@ -20,13 +26,29 @@ public class IAuthenticationServiceImpl extends IAuthenticationService.Stub {
 
     @Override
     public String getAuthenticationToken() throws RemoteException {
-        List<String>  grantedPermissions = getGrantedPermissions();
+        try {
+            List<String> grantedPermissions = getGrantedPermissions();
+            Log.d(TAG, "Permissions: "Arrays.toString(new List[]{grantedPermissions}));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
         // TODO Generate JWT with fetched permisions
         return null;
     }
 
-    private List<String> getGrantedPermissions() {
-        // TODO Connect to PackageManager and retrieve permissions;
-        return new ArrayList<String>();
+    private List<String> getGrantedPermissions() throws PackageManager.NameNotFoundException {
+        PackageManager packageManager = mContext.getPackageManager();
+        PackageInfo packageInfo = packageManager.getPackageInfo(packageManager.getNameForUid(
+                Binder.getCallingUid()), PackageManager.GET_PERMISSIONS);
+
+        List<String> permissionList = new ArrayList<>();
+        // requestedPermissions array contains both granted and non-granted permissions
+        // using corresponding requestedPermissionsFlags array, filter out the non-granted ones
+        for (int i = 0; i < packageInfo.requestedPermissions.length; i++) {
+            if ((packageInfo.requestedPermissionsFlags[i] & PackageInfo.REQUESTED_PERMISSION_GRANTED) == PackageInfo.REQUESTED_PERMISSION_GRANTED) {
+                permissionList.add(packageInfo.requestedPermissions[i]);
+            }
+        }
+        return permissionList;
     }
 }
